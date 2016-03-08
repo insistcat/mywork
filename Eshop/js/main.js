9b64dcdfb2f5c9a188d47398d029e27c33e34e50
insistcat <316861054@qq.com>
2016-03-08 11:40:11 +0800

    add the search text reset

--- Eshop/js/main.js
+++ Eshop/js/main.js
@@ -1,12 +1,32 @@
-window.onload()=function(){
-	
+window.onload=function(){
+	buy.app.toText();
 };
-var  buy={};
+var  buy={};	//命名空间
 
 buy.tools={};
 
 buy.ui={};
 
+buy.ui.toTextReset=function(obj,str){
+	obj.onfocus=function(){
+		if(obj.value == str){
+			obj.value="";
+		};
+	};
+	obj.onblur=function(){
+		if(obj.value == ""){
+			obj.value=str;
+		};
+	};
+};
+
 buy.app={
-	var arr=document.getElmentsClassName("Search");
+
+};
+buy.app.toText=function(){
+	var oTextUp=document.getElementById("sTextUp");
+	var oTextDown=document.getElementById("sTextDown");
+
+	buy.ui.toTextReset(oTextUp,"Search Website");
+	buy.ui.toTextReset(oTextDown,"Search Website");
 };
